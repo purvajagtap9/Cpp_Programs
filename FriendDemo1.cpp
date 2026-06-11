a06e17b90e64e4d9253b5e81f6968033580b4abc
purvajagtap9 <purvajagtap9@gmail.com>
2026-06-11 12:19:48 +0530

    Add files via upload

---
+++ FriendDemo1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    public : int i;
+    private : int j;
+    protected : int k;
+
+    public : 
+    Demo()
+    {   
+        i = 11;
+        j = 21;
+        k = 51;
+
+    }
+
+};
+
+void fun()
+{
+    Demo dobj;
+    cout<<dobj.i<<endl;
+    cout<<dobj.j<<endl;  //error
+    cout<<dobj.k<<endl;  //error
+
+}
+int main()
+{
+    fun();
+    return 0;
+}
